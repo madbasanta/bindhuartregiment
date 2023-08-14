d297907747a5a24c053173f0e673f184878915f2
madbasanta <42416735+madbasanta@users.noreply.github.com>
2023-08-14 23:24:43 +0545

    landing

--- public/index.php
+++ public/index.php
@@ -1,3 +1,25 @@
 <?php
 
+function base_dir($dir = '') {
+    $base = dirname(__DIR__, 2);
+
+    if($dir === '') {
+        return $base;
+    }
+
+    return $base . '/' . ltrim($dir, '\\/');
+}
+
+function dd() {
+    echo '<pre>';
+    foreach(func_get_args() as $arg) {
+        print_r($arg);
+    }
+    echo '</pre>';
+    exit;
+}
+
+
+dd($_SERVER);
+
 echo "Welcome!!";
