1fdc9b483882d99e4ad0bae4cd21689e3c064ee5
madbasanta <42416735+madbasanta@users.noreply.github.com>
2023-08-15 00:10:27 +0545

    logic remain

--- admin/login.php
+++ admin/login.php
@@ -3,6 +3,6 @@
 
 if($_SERVER['REQUEST_METHOD'] === 'GET') {
     require_once(base_path('backend/login.html'));
+} elseif($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // logic here
 }
-
-dd($_SERVER);
