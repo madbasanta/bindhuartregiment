6a857c0c297e190746d049495f706eafde35096e
madbasanta <tadashy444@gmail.com>
2023-10-10 23:36:03 +0545

    alter podcast

--- adminadmin/database/03table_podcasts.php
+++ adminadmin/database/03table_podcasts.php
@@ -19,3 +19,12 @@ $schema->timestamp('updated_at')->nullable();
 echo '⛏ Creating table podcasts...' . PHP_EOL;
 ORM::schema($schema->generateSQL());
 echo '✔ Created table podcasts' . PHP_EOL;
+
+$schema->addColumnIfNotExists('shortdesc', 'text', ['after description'])->nullable();
+$schema->addColumnIfNotExists('soundcloud_url', 'varchar(255)', ['after audio_file_path'])->nullable();
+$schema->addColumnIfNotExists('google_url', 'varchar(255)', ['after soundcloud_url'])->nullable();
+$schema->addColumnIfNotExists('podcast_date', 'date', ['after user_id'])->nullable();
+
+echo '⛏ Altering table podcasts...' . PHP_EOL;
+ORM::schema($schema->generateAddColumnSQL());
+echo '✔ Altered table podcasts' . PHP_EOL;
