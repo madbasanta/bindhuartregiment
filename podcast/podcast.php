d334cf6c8f281baf8f49000ac08840b3481a6cc8
madbasanta <tadashy444@gmail.com>
2023-10-12 00:40:01 +0545

    soundclound player

--- podcast/podcast.php
+++ podcast/podcast.php
@@ -368,11 +368,11 @@ if (empty($podcast)) {
                 const groups = decodeURIComponent(div.firstChild.src).match(/^.*\/\/api\.soundcloud\.com\/(?<type>[[a-z]*)\/(?<id>\d*)/).groups;
                 const embededCode = `<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" 
                 src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${groups.id}&color=%23ff5500&auto_play=true&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=false"></iframe>
-                <div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;">
-                    <a href="${data.author_url}" title="${data.author_name}" target="_blank" style="color: #cccccc; text-decoration: none;">
+                <div style="font-size: 10px; color: #333;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;">
+                    <a href="${data.author_url}" title="${data.author_name}" target="_blank" style="color: #333; text-decoration: none;">
                         ${data.author_name}
                     </a> ·
-                    <a href="<?= $podcast->soundcloud_url ?>" title="${data.title}" target="_blank" style="color: #cccccc; text-decoration: none;">
+                    <a href="<?= $podcast->soundcloud_url ?>" title="${data.title}" target="_blank" style="color: #333; text-decoration: none;">
                         ${data.title}
                     </a>
                 </div>`;
